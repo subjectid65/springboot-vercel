2a7ac7960fc39d29b68b1e96257819f84dfd2cea
subjectid65 <83794568+subjectid65@users.noreply.github.com>
2026-06-02 12:36:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const{ createServer }=require('vercel-java');
-const app=require('./public/root.war');
-module.exports.handler=createServer(app);
+const { createServer } = require('vercel-java');
+const app = require('./public/root.war');
+module.exports.handler = createServer(app);
